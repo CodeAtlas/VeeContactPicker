3fc7cd60606031762cb4370a30537bc02521baab
Raiden996 <andreacipriani89@gmail.com>
2016-03-14 16:49:32 +0100

    VeeContactPicker strings moved out in a new class

---
+++ VeeContactPicker/Classes/Model/VeeContactPickerStrings.h
@@ -0,0 +1,18 @@
+//
+//  VeeContactPickerStrings.h
+//  VeeContactPicker
+//
+//  Created by Andrea Cipriani on 14/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//Subclass it to override and chose your localized strings
+
+@interface VeeContactPickerStrings : NSObject
+
++(NSString*)localizedTitle;
++(NSString*)localizedCancelButtonTitle;
+
+@end
---
+++ VeeContactPicker/Classes/Model/VeeContactPickerStrings.m
@@ -0,0 +1,23 @@
+//
+//  VeeContactPickerStrings.m
+//  VeeContactPicker
+//
+//  Created by Andrea Cipriani on 14/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import "VeeContactPickerStrings.h"
+
+@implementation VeeContactPickerStrings
+
++(NSString*)localizedTitle
+{
+    return @"Choose a contact";
+}
+
++(NSString*)localizedCancelButtonTitle
+{
+    return @"Cancel";
+}
+
+@end
