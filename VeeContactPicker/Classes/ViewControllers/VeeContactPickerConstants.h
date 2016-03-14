c2e21978f81c50d9dbde77c123ea7d381979250c
Raiden996 <andreacipriani89@gmail.com>
2016-03-14 16:41:23 +0100

    VeeContactPicker constants moved out in a new class

---
+++ VeeContactPicker/Classes/ViewControllers/VeeContactPickerConstants.h
@@ -0,0 +1,17 @@
+//
+//  VeeContactPickerConstants.h
+//  VeeContactPicker
+//
+//  Created by Andrea Cipriani on 14/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VeeContactPickerConstants : NSObject
+
+extern NSString* const kVeeContactCellNibName;
+extern NSString* const kVeeContactCellIdentifier;
+extern NSInteger const kVeeContactCellHeight;
+
+@end
---
+++ VeeContactPicker/Classes/ViewControllers/VeeContactPickerConstants.m
@@ -0,0 +1,17 @@
+//
+//  VeeContactPickerConstants.m
+//  VeeContactPicker
+//
+//  Created by Andrea Cipriani on 14/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import "VeeContactPickerConstants.h"
+
+@implementation VeeContactPickerConstants
+
+NSString* const kVeeContactCellNibName = @"VeeContactUITableViewCell";
+NSString* const kVeeContactCellIdentifier = @"VeeContactCell"; //Also referenced into the xib
+NSInteger const kVeeContactCellHeight = 60.0;
+
+@end
