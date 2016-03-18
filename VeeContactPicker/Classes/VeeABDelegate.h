eb235b78af6d7149bf52118581c8c5360983bef0
Raiden996 <andreacipriani89@gmail.com>
2016-03-18 14:16:15 +0100

    Added VeeABDelegate

---
+++ VeeContactPicker/Classes/VeeABDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VeeABDelegate.h
+//  VeeContactPicker
+//
+//  Created by Andrea Cipriani on 18/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol VeeABDelegate <NSObject>
+
+-(void)abPermissionsGranted:(BOOL)granted;
+
+@end
