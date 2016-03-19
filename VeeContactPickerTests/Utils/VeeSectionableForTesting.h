329fc252c3e97326b0a34d0423d95de61067a41a
Raiden996 <andreacipriani89@gmail.com>
2016-03-19 17:23:43 +0100

    Added new class VeeSectionableForTesting

---
+++ VeeContactPickerTests/Utils/VeeSectionableForTesting.h
@@ -0,0 +1,13 @@
+//
+//  Created by Andrea Cipriani on 19/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "VeeSectionable.h"
+
+@interface VeeSectionableForTesting : NSObject <VeeSectionable>
+
+@property (nonatomic,copy) NSString* sectionIdentifier;
+
+@end
---
+++ VeeContactPickerTests/Utils/VeeSectionableForTesting.m
@@ -0,0 +1,10 @@
+//
+//  Created by Andrea Cipriani on 19/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import "VeeSectionableForTesting.h"
+
+@implementation VeeSectionableForTesting
+
+@end
