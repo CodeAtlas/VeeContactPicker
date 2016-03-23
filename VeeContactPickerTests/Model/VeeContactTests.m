d2c9e80e71a376188115466a0a83b16077e2757f
Raiden996 <andreacipriani89@gmail.com>
2016-03-23 12:13:58 +0100

    Fixed veeContact unit tests

--- VeeContactPickerTests/Model/VeeContactTests.m
+++ VeeContactPickerTests/Model/VeeContactTests.m
@@ -6,50 +6,29 @@
 //  Copyright © 2016 Code Atlas SRL. All rights reserved.
 //
 
-#import "VeeAddressBookForTesting.h"
-#import "VeeAddressBookForTestingConstants.h"
 #import "VeeContact.h"
 #import "VeecontactsForTestingFactory.h"
+#import "VeeAddressBookForTestingConstants.h"
 #import "XCTest+VeeCommons.h"
-#import <OCMock/OCMock.h>
 #import <XCTest/XCTest.h>
 
 @interface VeeContactTests : XCTestCase
 
 @property (nonatomic, strong) VeeContact* veeContactComplete;
-@property (nonatomic, strong) VeeContact* veeContactUnified;
-@property (nonatomic, strong) NSArray* veeContactsForTesting;
+@property (nonatomic, strong) NSArray* randomVeeContacts;
 
 @end
 
-static VeeAddressBookForTesting* veeAddressBookForTesting;
-static VeeContactsForTestingFactory* veeContactsForTestingFactory;
 
 @implementation VeeContactTests
-
-#pragma mark - Class setup
-
-+ (void)setUp
-{
-    veeAddressBookForTesting = [VeeAddressBookForTesting new];
-    veeContactsForTestingFactory = [[VeeContactsForTestingFactory alloc] initWithAddressBookForTesting:veeAddressBookForTesting];
-    [veeAddressBookForTesting deleteVeeTestingContactsFromAddressBook];
-    [veeAddressBookForTesting addVeeTestingContactsToAddressBook];
-}
-
-+ (void)tearDown
-{
-    [veeAddressBookForTesting deleteVeeTestingContactsFromAddressBook];
-}
 
 #pragma mark - Tests setup
 
 - (void)setUp
 {
     [super setUp];
-    _veeContactComplete = [veeContactsForTestingFactory veeContactComplete];
-    _veeContactUnified = [veeContactsForTestingFactory veeContactUnified];
-    _veeContactsForTesting = [veeContactsForTestingFactory veeContactsFromAddressBookForTesting];
+    _veeContactComplete = [VeeContactsForTestingFactory veeContactComplete];
+    _randomVeeContacts = [VeeContactsForTestingFactory createRandomVeeContacts:25];
 }
 
 - (void)tearDown
@@ -57,48 +36,7 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [super tearDown];
 }
 
-#pragma mark - Complete Veecontact tests
-
-- (void)testVeeContactCompleteCreation
-{
-    NSAssert(_veeContactComplete, @"VeeContact Complete creation failed");
-}
-
-- (void)testVeeContactCompleteFirstName
-{
-    BOOL isFirstNameCorrect = [_veeContactComplete.firstName isEqualToString:kCompleteVeeContactFirstName];
-    NSAssert(isFirstNameCorrect, @"VeeContact firstName is %@ but should be %@", _veeContactComplete.firstName, kCompleteVeeContactFirstName);
-}
-
-- (void)testVeeContactCompleteMiddleName
-{
-    BOOL isMiddleNameCorrect = [_veeContactComplete.middleName isEqualToString:kCompleteVeeContactMiddleName];
-    NSAssert(isMiddleNameCorrect, @"VeeContact middleName is %@ but should be %@", _veeContactComplete.middleName, kCompleteVeeContactMiddleName);
-}
-
-- (void)testVeeContactCompleteLastName
-{
-    BOOL isLastNameCorrect = [_veeContactComplete.lastName isEqualToString:kCompleteVeeContactLastName];
-    NSAssert(isLastNameCorrect, @"VeeContact lastName is %@ but should be %@", _veeContactComplete.lastName, kCompleteVeeContactLastName);
-}
-
-- (void)testVeeContactCompleteNickname
-{
-    BOOL isNickNameCorrect = [_veeContactComplete.nickname isEqualToString:kCompleteVeeContactNickname];
-    NSAssert(isNickNameCorrect, @"VeeContact nickname is %@ but should be %@", _veeContactComplete.nickname, kCompleteVeeContactNickname);
-}
-
-- (void)testVeeContactCompleteOrganizationName
-{
-    BOOL isveeContactCompleteOrganizationNameCorrect = [_veeContactComplete.organizationName isEqualToString:kCompleteVeeContactOrganizationName];
-    NSAssert(isveeContactCompleteOrganizationNameCorrect, @"VeeContact organizationName is %@ but should be %@", _veeContactComplete.organizationName, kCompleteVeeContactOrganizationName);
-}
-
-- (void)testVeeContactCompleteCompositeName
-{
-    BOOL isCompositeNameCorrect = [_veeContactComplete.compositeName isEqualToString:kCompleteVeeContactCompositeName];
-    NSAssert(isCompositeNameCorrect, @"VeeContact compositeName is %@ but should be %@", _veeContactComplete.compositeName, kCompleteVeeContactCompositeName);
-}
+#pragma mark - Display name
 
 - (void)testVeeContactCompleteDisplayName
 {
@@ -109,19 +47,20 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
 - (void)testVeeContactCompleteDisplayNameWithoutFirstName
 {
     [self nullifyIvarWithName:@"firstName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.organizationName;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
 
+
 - (void)testVeeContactCompleteDisplayNameWithoutLastName
 {
     [self nullifyIvarWithName:@"lastName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.organizationName;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
@@ -130,9 +69,9 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
 {
     [self nullifyIvarWithName:@"firstName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.lastName;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
@@ -141,9 +80,9 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
 {
     [self nullifyIvarWithName:@"lastName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.firstName;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
@@ -153,9 +92,9 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [self nullifyIvarWithName:@"firstName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"lastName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.middleName;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
@@ -166,9 +105,9 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [self nullifyIvarWithName:@"lastName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"middleName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-
+    
     NSString* aspectedDisplayName = _veeContactComplete.nickname;
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
 }
@@ -180,7 +119,7 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [self nullifyIvarWithName:@"middleName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"nickname" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-
+    
     BOOL isDisplayNameCorrect = [self isEmailAddress:_veeContactComplete.displayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be an email address", _veeContactComplete.displayName);
 }
@@ -192,124 +131,19 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [self nullifyIvarWithName:@"middleName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"nickname" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-    [self nullifyIvarWithName:@"emailsMutable" ofObject:_veeContactComplete];
-
+    [self nullifyIvarWithName:@"emails" ofObject:_veeContactComplete];
+    
     NSString* aspectedDisplayName = @"";
-
+    
     BOOL isDisplayNameCorrect = [_veeContactComplete.displayName isEqualToString:aspectedDisplayName];
     NSAssert(isDisplayNameCorrect, @"VeeContact displayName is %@ but should be %@", _veeContactComplete.displayName, aspectedDisplayName);
-}
-
-- (void)testVeecontactCompletePhoneNumbersCount
-{
-    BOOL isPhoneNumbersCountCorrect = [_veeContactComplete.phoneNumbers count] == kCompleteVeeContactPhoneNumbersCount;
-    NSAssert(isPhoneNumbersCountCorrect, @"VeeContact complete phone numbers count is %zd but should be %zd", [_veeContactComplete.phoneNumbers count], kCompleteVeeContactPhoneNumbersCount);
-}
-
-- (void)testVeecontactCompleteEmailsCount
-{
-    BOOL isEmailsCountCorrect = [_veeContactComplete.emails count] == kCompleteVeeContactEmailsCount;
-    NSAssert(isEmailsCountCorrect, @"VeeContact complete emails count is %zd but should be %zd", [_veeContactComplete.emails count], kCompleteVeeContactEmailsCount);
-}
-
-#pragma mark - Unified Veecontact
-
-//TODO: ... Can't test unified contacts because I can't reproduce them (linked records) in the address book programmatically, see http://stackoverflow.com/questions/24224929/is-there-a-way-to-programmatically-create-linked-contact
-
-#pragma mark - VeecontactsForTesting tests
-
-- (void)testVeeContactsCreationCount
-{
-    BOOL isVeeContactsCountCorrect = [_veeContactsForTesting count] == kVeeTestingContactsNumber;
-    NSAssert(isVeeContactsCountCorrect, @"Loaded %zd contacts from abForTesting, but they should be %zd", [_veeContactsForTesting count], kVeeTestingContactsNumber);
-}
-
-- (void)testVeeContactsRecordIdExist
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        BOOL hasAtLeastOneRecordId = [[veeContact recordIds] count] > 0;
-        NSAssert(hasAtLeastOneRecordId, @"VeeContact %@ has no recordIds", veeContact.displayName);
-    }
-}
-
-- (void)testVeecontactsCreatedAtExist
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        NSAssert(veeContact.createdAt, @"VeeContact unified has no createdAt date");
-    }
-}
-
-- (void)testVeecontactsModifiedAtExist
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        NSAssert(veeContact.modifiedAt, @"VeeContact unified has no modifiedAt date");
-    }
-}
-
-- (void)testVeecontactsCreatedAtCantBeAfterModifiedAt
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        BOOL isVeeContactCreatedBeforeOrEqualeModifiedAt = [veeContact.createdAt compare:veeContact.modifiedAt] == NSOrderedSame || [veeContact.createdAt compare:veeContact.modifiedAt] == NSOrderedAscending;
-        NSAssert(isVeeContactCreatedBeforeOrEqualeModifiedAt, @"Veecontact %@ createdAt %@, that is after its modifiedAt: %@", veeContact.displayName, veeContact.createdAt, veeContact.modifiedAt);
-    }
-}
-
-- (void)testVeecontactsImageCount
-{
-    NSUInteger veecontactsImageCount = 0;
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        if (veeContact.thumbnailImage) {
-            veecontactsImageCount++;
-        }
-    }
-
-    BOOL isVeecontactsImageNumberCorrect = veecontactsImageCount == kVeeTestingContactsWithImage;
-    NSAssert(isVeecontactsImageNumberCorrect, @"Veecontacts with image are %zd, but they should be %zd", veecontactsImageCount, kVeeTestingContactsWithImage);
-}
-
-- (void)testVeecontactsPhoneNumbersCount
-{
-    NSUInteger phoneNumberCount = 0;
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        phoneNumberCount += [[veeContact phoneNumbers] count];
-    }
-
-    BOOL isPhoneNumberCountCorrect = phoneNumberCount == kVeeTestingContactsPhoneNumbersCount;
-    NSAssert(isPhoneNumberCountCorrect, @"Veecontacts phone numbers are %ld, but they should be %ld", phoneNumberCount, kVeeTestingContactsPhoneNumbersCount);
-}
-
-- (void)testVeecontactsPhoneNumberDuplicate
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        NSSet* phoneNumberSet = [NSSet setWithArray:veeContact.phoneNumbers];
-        NSAssert([phoneNumberSet count] == [veeContact.phoneNumbers count], @"Veecontacts phone numbers contain duplicates!");
-    }
-}
-
-- (void)testVeecontactsEmailsCount
-{
-    NSUInteger emailsCount = 0;
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        emailsCount += [[veeContact emails] count];
-    }
-
-    BOOL isEmailCountCorrect = emailsCount == kVeeTestingContactsEmailsCount;
-    NSAssert(isEmailCountCorrect, @"Veecontacts phone numbers are %ld, but they should be %ld", emailsCount, kVeeTestingContactsEmailsCount);
-}
-
-- (void)testVeecontactsEmailsNoDuplicate
-{
-    for (VeeContact* veeContact in _veeContactsForTesting) {
-        NSSet* emailSet = [NSSet setWithArray:veeContact.emails];
-        NSAssert([emailSet count] == [veeContact.emails count], @"VeeContacts emails contain duplicates!");
-    }
 }
 
 #pragma mark - Section identifier
 
 - (void)testVeecontactsSectionIdentifierShouldBeOneCharacter
 {
-    for (VeeContact* veeContact in _veeContactsForTesting) {
+    for (VeeContact* veeContact in _randomVeeContacts) {
         NSString* veeContactSectionIdentifier = [veeContact sectionIdentifier];
         NSAssert(veeContactSectionIdentifier, @"VeeContact %@ has no section identifier", veeContact.displayName);
         BOOL isSectionIdentiferOneCharacter = [veeContactSectionIdentifier length] == 1;
@@ -347,7 +181,7 @@ static VeeContactsForTestingFactory* veeContactsForTestingFactory;
     [self nullifyIvarWithName:@"middleName" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"nickname" ofObject:_veeContactComplete];
     [self nullifyIvarWithName:@"organizationName" ofObject:_veeContactComplete];
-    [self nullifyIvarWithName:@"emailsMutable" ofObject:_veeContactComplete];
+    [self nullifyIvarWithName:@"emails" ofObject:_veeContactComplete];
 
     NSString* aspectedSectionIdentifier = _veeContactComplete.sectionIdentifier;
     
