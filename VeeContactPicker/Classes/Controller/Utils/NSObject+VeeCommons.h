7be75b2e8d2b0c6b0e26b4e6d22d9ee8bab7f30e
Raiden996 <andreacipriani89@gmail.com>
2016-03-30 12:21:14 +0200

    Added VeeCommons category with a method to format the description of an array.

---
+++ VeeContactPicker/Classes/Controller/Utils/NSObject+VeeCommons.h
@@ -0,0 +1,12 @@
+//
+//  Created by Andrea Cipriani on 30/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (VeeCommons)
+
+- (NSString*)formattedDescriptionOfArray:(NSArray*)array;
+
+@end
---
+++ VeeContactPicker/Classes/Controller/Utils/NSObject+VeeCommons.m
@@ -0,0 +1,30 @@
+//
+//  Created by Andrea Cipriani on 30/03/16.
+//  Copyright © 2016 Code Atlas SRL. All rights reserved.
+//
+
+#import "NSObject+VeeCommons.h"
+
+@implementation NSObject (VeeCommons)
+
+- (NSString*)formattedDescriptionOfArray:(NSArray*)array
+{
+    if (array == nil || [array count] == 0) {
+        return @"[]";
+    }
+
+    NSMutableString* arrayDescriptionMutable = [[NSMutableString alloc] initWithString:@"["];
+    for (id obj in array) {
+        [arrayDescriptionMutable appendString:[NSString stringWithFormat:@"%@", [obj description]]];
+        BOOL isNotLastObject = [obj isEqual:[array lastObject]] == NO;
+        if (isNotLastObject) {
+            [arrayDescriptionMutable appendString:@"; "];
+        }
+        else {
+            [arrayDescriptionMutable appendString:@"]"];
+        }
+    }
+    return [NSString stringWithString:arrayDescriptionMutable];
+}
+
+@end
