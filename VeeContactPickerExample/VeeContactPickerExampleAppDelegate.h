5a0c6135ffa14e1834732078f091e01a6aacb5db
Raiden996 <andreacipriani89@gmail.com>
2016-04-14 15:55:54 +0200

    Example: renamed AppDelegate in VeeContactAppDelegate

--- VeeContactPickerExample/VeeContactPickerExampleAppDelegate.h
+++ VeeContactPickerExample/VeeContactPickerExampleAppDelegate.h
@@ -1,14 +1,10 @@
-//
-//  AppDelegate.h
-//  VeeContactPicker
-//
 //  Created by Andrea Cipriani on 21/12/15.
 //  Copyright © 2015 Code Atlas SRL. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface VeeContactPickerExampleAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- VeeContactPickerExample/VeeContactPickerExampleAppDelegate.m
+++ VeeContactPickerExample/VeeContactPickerExampleAppDelegate.m
@@ -1,18 +1,15 @@
 //
-//  AppDelegate.m
-//  VeeContactPicker
-//
 //  Created by Andrea Cipriani on 21/12/15.
 //  Copyright © 2015 Code Atlas SRL. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "VeeContactPickerExampleAppDelegate.h"
 
-@interface AppDelegate ()
+@interface VeeContactPickerExampleAppDelegate ()
 
 @end
 
-@implementation AppDelegate
+@implementation VeeContactPickerExampleAppDelegate
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
