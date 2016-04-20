21421a81a84f027876d08ac50c80bcbe9596d93a
Raiden996 <andreacipriani89@gmail.com>
2016-04-20 13:12:13 +0200

    Minor changes

--- Pod/Classes/Controller/ViewController/VeeContactPickerViewController.h
+++ Pod/Classes/Controller/ViewController/VeeContactPickerViewController.h
@@ -17,7 +17,7 @@
 
 #pragma mark - Init
 
-//If veeContacts are not set, the picker will show all contacts of the address book
+//If veeContacts are not set, the picker will show the contacts from the address book
 - (instancetype)initWithDefaultConfiguration;
 - (instancetype)initWithOptions:(VeeContactPickerOptions*)veeContactPickerOptions;
 - (instancetype)initWithVeeContacts:(NSArray<id<VeeContactProt>>*)veeContacts;
@@ -28,7 +28,7 @@
 @property (nonatomic, strong) id<VeeContactPickerDelegate> contactPickerDelegate;
 @property (nonatomic, strong) void (^contactSelectionHandler)(id<VeeContactProt>);
 
-#pragma mark - Outlets
+#pragma mark - Public Outlets
 
 @property (nonatomic, strong) IBOutlet UITableView* contactsTableView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem* cancelBarButtonItem;
